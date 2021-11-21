62b8074396732d613b140c8ffc752b725c2b916d
michaski <michaski86@gmail.com>
2021-11-21 22:07:44 +0100

    Added basic Header component

---
+++ Source/BandClickFrontend/band-click-frontend/src/components/header/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {useHistory} from "react-router";
+
+const Header = () => {
+    const history = useHistory();
+    const redirectTo = (path: string, name: string) =>
+        <a className="nav-link p-2" onClick={() => { history.push(path) }}>{name}</a>
+    return (
+        <header className="row">
+            <nav className="navbar navbar-expand-lg ">
+                <div className="container-fluid">
+                    <div>
+                        <a className="navbar-brand d-inline-flex" href="/">
+                            <span className="fw-bold">Band Click</span>
+                        </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="collapse navbar-collapse d-inline-flex justify-content-end" id="navbarNavAltMarkup">
+                        <div className="navbar">
+                            {redirectTo('/', 'Home')}
+                            {redirectTo('login', 'Login')}
+                            {redirectTo('register', 'Register')}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ Source/BandClickFrontend/band-click-frontend/src/views/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+const Home = () => {
+    return (
+        <>
+        <Header />
+        <h1>This is home view</h1>
+        </>
+    );
+}
+
+export default Home;
