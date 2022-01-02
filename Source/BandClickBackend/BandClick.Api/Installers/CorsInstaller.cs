51c36613377a20e20877354bd72356d3f65db82a
michaski <michaski86@gmail.com>
2022-01-02 20:54:36 +0100

    Added CORS policy

---
+++ Source/BandClickBackend/BandClick.Api/Installers/CorsInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BandClickBackend.Api.Installers
+{
+    public class CorsInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("BandClickCorsPolicy", builder => 
+                    builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithOrigins(Configuration["AllowedOrigins"])
+                    );
+            });
+        }
+    }
+}
