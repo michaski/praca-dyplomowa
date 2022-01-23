7d8c0af853b15b48095b7ebe9d0b1eaddc2dae08
michaski <michaski86@gmail.com>
2022-01-23 14:40:21 +0100

    Login view polish

--- Source/BandClickFrontend/band-click-frontend/src/views/login/Login.tsx
+++ Source/BandClickFrontend/band-click-frontend/src/views/login/Login.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
+import { Col, FloatingLabel, Form, Row } from "react-bootstrap";
 import Header from "../../components/header/Header";
 import auth from "../../services/auth/auth";
+import './login.css';
 
 const Login = (props: any) => {
     let email = '';
@@ -13,34 +15,34 @@ const Login = (props: any) => {
     });
 
     return (
-        <div className="container-fluid">
+        <div className="container-fluid px-0">
             <Header />
             <div className="container">
-                <div className="row">
-                    <h1>Logowanie</h1>
-                </div>
-                <div className="row">
-                    <div className="mb-3 row">
-                        <label htmlFor="emailInput" className="col-sm-2 col-form-label">Email</label>
-                        <div className="col-sm-10">
-                            <input type="email" className="col-sm-10 form-control" id="emailInput" placeholder="name@example.com" onChange={(e) => email = e.target.value} />
-                        </div>
-                    </div>
-                    <div className="mb-3 row">
-                        <label htmlFor="passwordInput" className="col-sm-2 col-form-label">Hasło</label>
-                        <div className="col-sm-10">
-                            <input type="password" className="col-sm-10 form-control" id="passwordInput" onChange={(e) => password = e.target.value} />
-                        </div>
-                    </div>
-                    <div className="mb-3 row">
-                        <input type="submit" className="btn btn-primary" value="Login" onClick={async (e) => {
-                            await auth.login(email, password)
-                                .then(() => {
-                                    props.history.push('/app');
-                                });
-                        }} />
-                    </div>
-                </div>
+                <Form className="login-form" onSubmit={() => {
+                    auth.login(email, password)
+                    .then(() => {
+                        props.history.push('/app');
+                    });
+                }}>
+                    <Row>
+                        <h1>Logowanie</h1>
+                    </Row>
+                    <Row className="mb-3">
+                        <Form.Label htmlFor="emailInput" className="col-sm-2 col-form-label">Email</Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="email" className="col-sm-10 form-control" id="emailInput" placeholder="name@example.com" onChange={(e) => email = e.target.value} />   
+                        </Col>
+                    </Row>
+                    <Row className="mb-3">
+                        <Form.Label htmlFor="passwordInput" className="col-sm-2 col-form-label">Hasło</Form.Label>
+                        <Col sm="10">
+                            <Form.Control type="password" className="col-sm-10 form-control" id="passwordInput" placeholder="Hasło" onChange={(e) => password = e.target.value} />   
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Form.Control type="submit" role="button" className="btn btn-primary form-submit" value="Zaloguj się" />
+                    </Row>
+                </Form>
             </div>
         </div>
     );
